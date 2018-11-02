0d3f7c0ded39135e97fa315f08506a7c0748b0c4
Szymson <sz.orlowski95@gmail.com>
2018-11-02 22:30:45 +0100

    search vehicles by price

--- src/components/App.js
+++ src/components/App.js
@@ -12,107 +12,115 @@ import Login from "./Login";
 import Rent from "./Rent";
 
 class App extends Component {
-  setCampers = () => {
-    console.log("fetching campers...");
-    fetch(`http://localhost:8080/vehicles?type=c`)
-      .then(response => {
-        return response.json()
-      })
-      .then(response => {
-        let campers = this.state.campers;
-        campers.content = response;
-        this.setState({campers: campers});
-        console.log("fetching done")
-      });
-  };
-
-  setLimousines = () => {
-    console.log("fetching limousines...");
-    fetch(`http://localhost:8080/vehicles?type=l`)
-      .then(response => {
-        return response.json()
-      })
-      .then(response => {
-        let limousines = this.state.limousines;
-        limousines.content = response;
-        this.setState({limousines: limousines});
-        console.log("fetching done")
-      });
-  };
-
-  headerItems =
-    [
-      {name: 'Info', link: '/'}, {name: 'Campers', link: '/campers'}, {name: 'Limousines', link: '/limousines'},
-      {name: 'Gallery', link: '/gallery'}, {name: 'Contact', link: '/contact'}, {name: 'Login', link: '/login'}
-    ];
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: {
-        userId: undefined,
-        status: false
-      },
-      campers: {
-        content: []
-      },
-      limousines: {
-        content: []
-      },
-      info: {
-        desc: 'Opis najlepszej firmy ever'
-      },
-      contact: {
-        desc: {
-          phone: '+48 123 456 789',
-          email: 'super@email.com'
+    setCampers = (price) => {
+        console.log("fetching campers...");
+        let url = `http://localhost:8080/vehicles?type=l`;
+        if (price !== undefined) {
+            url = `http://localhost:8080/vehicles?type=c&price=` + price;
         }
-      }
+        fetch(url)
+            .then(response => {
+                return response.json()
+            })
+            .then(response => {
+                let campers = this.state.campers;
+                campers.content = response;
+                this.setState({campers: campers});
+                console.log("fetching done")
+            });
+    };
+
+    setLimousines = (price) => {
+        console.log("fetching limousines...");
+        let url = `http://localhost:8080/vehicles?type=l`;
+        if (price !== undefined) {
+            url = `http://localhost:8080/vehicles?type=l&price=` + price;
+        }
+        fetch(url)
+            .then(response => {
+                return response.json()
+            })
+            .then(response => {
+                let limousines = this.state.limousines;
+                limousines.content = response;
+                this.setState({limousines: limousines});
+                console.log("fetching done")
+            });
+    };
+
+    headerItems =
+        [
+            {name: 'Info', link: '/'}, {name: 'Campers', link: '/campers'}, {name: 'Limousines', link: '/limousines'},
+            {name: 'Gallery', link: '/gallery'}, {name: 'Contact', link: '/contact'}, {name: 'Login', link: '/login'}
+        ];
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: {
+                userId: undefined,
+                status: false
+            },
+            campers: {
+                content: []
+            },
+            limousines: {
+                content: []
+            },
+            info: {
+                desc: 'Opis najlepszej firmy ever'
+            },
+            contact: {
+                desc: {
+                    phone: '+48 123 456 789',
+                    email: 'super@email.com'
+                }
+            }
+        }
+
     }
 
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Route
-            path="/"
-            render={(props) => <Header {...props} items={this.headerItems}/>}/>
-          <div className="container">
-            <Route
-              exact path="/"
-              render={(props) => <Info {...props} info={this.state.info}/>}/>
-            <Route
-              exact path="/campers"
-              render={(props) => <Vehicles {...props} vehicles={this.state.campers}
-                                           fetchVehicles={() => this.setCampers()}/>}
-            />
-            <Route
-              exact path="/limousines"
-              render={(props) => <Vehicles {...props} vehicles={this.state.limousines}
-                                           fetchVehicles={() => this.setLimousines()}/>}
-            />
-            <Route
-              exact path="/contact"
-              render={(props) => <Contact {...props} contact={this.state.contact}
-                                          fetchVehicles={() => this.setLimousines()}/>}
-            />
-            <Route
-              exact path="/login"
-              render={props => <Login {...props}/>}/>
-            <Route
-              exact path="/rent"
-              render={props => <Rent {...props} userId={1}/>}/>
-          </div>
-          <Route
-            path="/"
-            component={Footer}
-          />
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Route
+                        path="/"
+                        render={(props) => <Header {...props} items={this.headerItems}/>}/>
+                    <div className="container">
+                        <Route
+                            exact path="/"
+                            render={(props) => <Info {...props} info={this.state.info}/>}/>
+                        <Route
+                            exact path="/campers"
+                            render={(props) => <Vehicles {...props} vehicles={this.state.campers}
+                                                         fetchVehicles={() => this.setCampers()}/>}
+                        />
+                        <Route
+                            exact path="/limousines"
+                            render={(props) => <Vehicles {...props} vehicles={this.state.limousines}
+                                                         fetchVehicles={(price) => this.setLimousines(price)}/>}
+                        />
+                        <Route
+                            exact path="/contact"
+                            render={(props) => <Contact {...props} contact={this.state.contact}
+                                                        fetchVehicles={(price) => this.setLimousines(price)}/>}
+                        />
+                        <Route
+                            exact path="/login"
+                            render={props => <Login {...props}/>}/>
+                        <Route
+                            exact path="/rent"
+                            render={props => <Rent {...props} userId={1}/>}/>
+                    </div>
+                    <Route
+                        path="/"
+                        component={Footer}
+                    />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -5,27 +5,37 @@ import '../styles/campers.css'
 import '../styles/content.css'
 
 const methods = {
-  componentDidMount(props){
-    var fetchMethod = props.fetchVehicles;
-    fetchMethod();
-  }
+    componentDidMount(props) {
+        let fetchMethod = props.fetchVehicles;
+        fetchMethod();
+    }
 };
 
 const Vehicles = props => {
-    return(
+
+    const updateVehicles = () => {
+        const price = document.getElementById("price").value;
+        console.log("Searching by price..." + price)
+        const fetchMethod = props.fetchVehicles;
+        fetchMethod(price);
+    };
+
+    return (
         <div className="info content" onClick={props.clicked}>
-          <div className="campers">
-            {props.vehicles.content.map(vehicle => {
-              return <Vehicle
-                key={Math.random()}
-                price={vehicle.price}
-                stars={vehicle.stars}
-                name={vehicle.name}
-                photo={vehicle.photo}
-                rented={vehicle.rented}
-                id={vehicle.id}/>
-            })}
-          </div>
+            <input id="price"/>
+            <button id="searchButton" onClick={updateVehicles}>SEARCH BY PRICE</button>
+            <div className="campers">
+                {props.vehicles.content.map(vehicle => {
+                    return <Vehicle
+                        key={Math.random()}
+                        price={vehicle.price}
+                        stars={vehicle.stars}
+                        name={vehicle.name}
+                        photo={vehicle.photo}
+                        rented={vehicle.rented}
+                        id={vehicle.id}/>
+                })}
+            </div>
         </div>
     )
 };
